4b38e78b9690a23aa74338d3599d0c6ff294de53
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-03-25 13:29:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
+
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+
+//Use morgan
+app.use(morgan('dev'))
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
