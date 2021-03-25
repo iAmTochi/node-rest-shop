838e620c0d05949b1c33ee0a84f3aea8f69cba5a
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-03-25 13:43:27 +0100

    handling errors for 404 not found

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/orders', orderRoutes);
 // handling errors
 app.use((req, res, next) =>{
     const error = new Error('Not found');
-    error.status(404);
+    error.status = 404;
     next(error);
 })
 
