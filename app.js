a4f4000bd9f36559f2675c755ae28e4ac7e4d165
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-03-25 12:39:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 
-
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
