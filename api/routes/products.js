de2371368db381687d6cd970b8882ebd530c736d
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-03-26 13:25:49 +0100

    implemented the update product

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,7 +8,13 @@ router.get('/', (req, res, next) => {
     Product.find().exec()
         .then(docs => {
             console.log(docs);
-            res.status(200).json(docs);
+            if(docs.length > 0){
+                res.status(200).json(docs);
+            } else {
+                res.status(404).json({
+                    message: "No entries found"
+                });
+            }
         })
         .catch(err => {
             console.log(err);
@@ -62,16 +68,37 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "Updated product",
-        
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch( err  => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "Deleted product!",
-    });
+    const id = req.params.productId;
+    Product.remove({_id: id}).exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 
